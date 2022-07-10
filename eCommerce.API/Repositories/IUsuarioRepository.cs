2362c1a5f779eb49bad82e87b7ccd1304d0fc00e
x340348 <Tty_Erick.Alencar@mercantil.com.br>
2022-07-10 18:27:43 -0300

    first commit

---
+++ eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        public List<Usuario> Get();
+        public Usuario GetById(int id);
+        public void Update(Usuario usuario);
+        public void Insert(Usuario usuario);
+        public void Delete(int id);
+
+    }
+}
